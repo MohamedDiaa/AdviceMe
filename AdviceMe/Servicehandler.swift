188788846e08c1dcd85a76879567d13ebe6d4026
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-01-04 19:17:45 +0100

    -Added Alamofire

---
+++ AdviceMe/Servicehandler.swift
@@ -0,0 +1,13 @@
+//
+//  Servicehandler.swift
+//  AdviceMe
+//
+//  Created by MohamedDiaa on 1/4/16.
+//  Copyright © 2016 MohamedDiaa. All rights reserved.
+//
+
+import UIKit
+
+class Servicehandler: NSObject {
+
+}
